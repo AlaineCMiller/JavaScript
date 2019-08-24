c4c8a10329253f23587d602b01ff0e21c45d1e3e
Alaine Miller <44164856+AlaineCMiller@users.noreply.github.com>
2019-08-24 21:39:31 +0100

    Add files via upload

---
+++ Exercism/resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,6 @@
+
+export const COLORS = ['black','brown','red','orange','yellow','green','blue','violet','grey','white'];
+
+export function value ([color1, color2]) {
+  return parseInt(`${COLORS.indexOf(color1)}${COLORS.indexOf(color2)}`)};
+
