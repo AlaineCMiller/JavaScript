94b0445285ff3cdf6bc9c3c77648f079d99ce1bb
Alaine Miller <alaine.miller@bbc.co.uk>
2020-10-12 21:44:05 +0100

    notes-app arrow function refactor

--- node-course/notes-app/notes.js
+++ node-course/notes-app/notes.js
@@ -1,15 +1,13 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-const getNotes = function () {
+const getNotes = () => {
    return 'Your notes...'
 }
 
-const addNote = function (title, body) {
+const addNote = (title, body) => {
    const notes = loadNotes()
-   const duplicateNotes = notes.filter(function(note){
-      return note.title === title
-   })
+   const duplicateNotes = notes.filter((note) => note.title === title)
 
    if (duplicateNotes.length === 0) {
       notes.push({
@@ -23,11 +21,10 @@ const addNote = function (title, body) {
    }
 }
 
-const removeNote = function (title){
+const removeNote = (title) => {
    const notes = loadNotes()
-   const notesToKeep = notes.filter(function(note){
-      return note.title !== title
-   })
+   const notesToKeep = notes.filter((note) => note.title !== title)
+   
 
    if (notes.length > notesToKeep.length) {
       console.log(chalk.green.inverse('Note removed!'))
@@ -37,14 +34,24 @@ const removeNote = function (title){
    }
 }
    
+const listNotes = () => {
+   const notes = loadNotes()
+   
+   console.log(chalk.inverse('Your Notes'))
+
+   notes.forEach((note) => {
+      console.log(note.title)
+   })
+
+}
 
 
-const saveNotes = function (notes) {
+const saveNotes = (notes) => {
    const dataJSON = JSON.stringify(notes)
    fs.writeFileSync('notes.json', dataJSON)
 }
 
-const loadNotes = function () {
+const loadNotes = () => {
    try {
       const dataBuffer = fs.readFileSync('notes.json')
       const dataJSON = dataBuffer.toString()
@@ -58,5 +65,6 @@ const loadNotes = function () {
 module.exports = {
    getNotes: getNotes,
    addNote: addNote,
-   removeNote: removeNote
+   removeNote: removeNote,
+   listNotes: listNotes
 }
